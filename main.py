6f66f0c48a8edc4261adc89f0e944a0f6788496d
Code0Light1 <kirankk105@gmail.com>
2025-07-21 18:07:42 +0100

    Refactor

--- main.py
+++ main.py
@@ -44,20 +44,24 @@ def main():
     messages = [
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
-    system_prompt = """
-        You are a helpful AI coding agent.
-
-        When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
-
-        - List files and directories
-        - Read file contents
-        - Execute Python files with optional arguments
-        - Write or overwrite files
-
-        All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
-    """
     if verbose:
-        print(f'User prompt: {user_prompt}')   
+        print(f'User prompt: {user_prompt}')  
+    generate_content(client, messages, verbose)
+
+def generate_content(client, messages, verbose):
+    system_prompt = """
+    You are a helpful AI coding agent.
+
+    When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+    - List files and directories
+    - Read file contents
+    - Execute Python files with optional arguments
+    - Write or overwrite files
+
+    All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+    """
+     
     content_res = client.models.generate_content(
                     model='gemini-2.0-flash-001', 
                     contents=messages,
